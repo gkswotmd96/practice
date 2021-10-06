ebbc45ad2af500f97ee9e18150f3310a3673a7a6
gkswotmd96 <58037939+gkswotmd96@users.noreply.github.com>
2021-10-06 23:26:38 +0900

    Update 2475.java
    백준 2475번

--- 2475.java
+++ 2475.java
@@ -1,19 +1,19 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class Main {
-	public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Integer a = sc.nextInt(); 
-        Integer b = sc.nextInt();
-        Integer c = sc.nextInt();
-        Integer d = sc.nextInt();
-        Integer e = sc.nextInt();
-        int f = 0;
-        
-        f = ((a*a) + (b*b) + (c*c) + (d*d) + (e*e)) % 10;
-        
-        System.out.println(f);
-        sc.close();
+	public static void main(String[] args) throws IOException{
+
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		
+		for(int i=0; i<5; i++) {
+			int x = sc.nextInt();
+			sum += x*x;
+		}
+		
+		System.out.println(sum % 10);
+
 	}
 }
