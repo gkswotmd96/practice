18b3631a3b8f98341828d9ea3390acaa8b4e21b0
gkswotmd96 <58037939+gkswotmd96@users.noreply.github.com>
2021-10-06 23:46:38 +0900

    2914.java
    백준 2914번

--- 2845.java
+++ 2845.java
@@ -9,23 +9,8 @@ public class Main {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		
-		int[] arr = new int[5];
-		
-		for(int i =0; i < 5; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		
-		int sum = a*b;
-		
-		for(int i =0; i < 5; i++) {
-			int result = 0;
-			result = sum - arr[i];
-			System.out.print(-result+" ");
-		}
-		
+		System.out.println(a*(b-1)+1);
 		
 		sc.close();
-
 	}
 }
