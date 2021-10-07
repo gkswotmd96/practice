b7a5ac323190c7510023c28ca8461cad96b2de0b
gkswotmd96 <58037939+gkswotmd96@users.noreply.github.com>
2021-10-07 15:37:48 +0900

    Create 6749.java
    Next in line

---
+++ 6749.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int z = y - x;
+		int result = y+z;
+		System.out.println(result);
+		}
+}
