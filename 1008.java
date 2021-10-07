c5d4b65d2c69c87f2849317c26c096520f55bbb6
gkswotmd96 <58037939+gkswotmd96@users.noreply.github.com>
2021-10-07 18:30:21 +0900

    Create 1008.java
    double형(실수)를 이용한 나눗셈

---
+++ 1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		sc.close();
+		
+		System.out.println(x/y);
+		}
+}
