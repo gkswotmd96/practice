125c69a16be60533a406f7b9f9ac011f7f929faa
gkswotmd96 <58037939+gkswotmd96@users.noreply.github.com>
2022-01-05 16:22:38 +0900

    Create 17608a.java
    stack 사용

---
+++ 17608a.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class one_three {
+	
+	public static void main(String[] args) throws IOException{
+
+		Stack<Integer> stack = new Stack<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+	    int n = Integer.parseInt(str);
+		
+	    int h =0;
+	    for(int i =0; i<n; i++) {
+	    	str=br.readLine();
+	    	h = Integer.parseInt(str);
+	    	stack.push(h);
+	    }
+	    
+	    int standard = stack.pop();
+	    int cnt = 1;
+	    int current=0;
+	    while(!stack.isEmpty()) {
+	    	current = stack.pop();
+	    	if(current>standard) {
+	    		standard = current;
+	    		cnt++;
+	    	}
+	    }
+
+		System.out.print(cnt);
+		
+	    
+	}
+	
+}
+
+
